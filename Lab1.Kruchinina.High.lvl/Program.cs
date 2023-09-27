08c6b2b57fe18d54af5601cec1504c6b9b0742e8
NIK <NIK@DESKTOP-MH7JFSC>
2023-09-28 00:12:05 +0200

    Add project files.

---
+++ Lab1.Kruchinina.High.lvl/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("ВВедите c:");
+double c = double.Parse(Console.ReadLine());
+Console.Write("Введите q:");
+double q = double.Parse(Console.ReadLine());
+double a = Math.Sqrt((c * c) - (q*q));
+Console.WriteLine($"a={a:F2}");
+double s = (c * q) / 2;
+Console.WriteLine($"s={s:F2}");
